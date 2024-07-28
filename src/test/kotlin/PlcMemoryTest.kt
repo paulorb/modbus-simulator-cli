d31e90c401a77fa9f298012cb4c0d1cc4f8d18e7
paulorb <paulorbhell@gmail.com>
2024-07-28 11:09:27 +0400

    Added unit tests for PLCMemory

---
+++ src/test/kotlin/PlcMemoryTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class PlcMemoryTest {
+
+    @Test
+    fun `PlcMemory must be initialized with configured holding registers symbols`() {
+        val configuration = ConfigurationParser()
+        configuration.setReadFromResources(true)
+        configuration.setFileName("configuration_init.xml")
+        val plcMemory = PlcMemory(configuration)
+        val values = plcMemory.readHoldingRegister(14, 1)
+        assertEquals(1, values.size, )
+        assertEquals( values[0], 500)
+    }
+}
