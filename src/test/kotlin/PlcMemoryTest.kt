4f622d001693b49c996ca6d4e9d6096b692fd08b
paulorb <paulorbhell@gmail.com>
2024-07-28 11:19:06 +0400

    Added unit tests for PLCMemory

--- src/test/kotlin/PlcMemoryTest.kt
+++ src/test/kotlin/PlcMemoryTest.kt
@@ -14,4 +14,15 @@ class PlcMemoryTest {
         assertEquals(1, values.size, )
         assertEquals( values[0], 500)
     }
+
+    @Test
+    fun `PlcMemory must be initialized with configured input registers symbols`() {
+        val configuration = ConfigurationParser()
+        configuration.setReadFromResources(true)
+        configuration.setFileName("configuration_init.xml")
+        val plcMemory = PlcMemory(configuration)
+        val values = plcMemory.readInputRegister(5, 1)
+        assertEquals(1, values.size, )
+        assertEquals( values[0], 30)
+    }
 }
