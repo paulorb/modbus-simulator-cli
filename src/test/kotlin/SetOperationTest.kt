583949a574f2fec1b0bc8803daae433078c76ce8
paulorb <paulorbhell@gmail.com>
2024-12-09 22:40:04 +0400

    Added unit tests for Set operation

--- src/test/kotlin/SetOperationTest.kt
+++ src/test/kotlin/SetOperationTest.kt
@@ -16,4 +16,18 @@ class SetOperationTest {
         setOperation.setOperation(configuration.getConfiguredDevice().simulation.randomElements[0] as Set)
         assertEquals(plcMemory.readCoilStatus(configuration.getConfiguredDevice().configuration.registers.register.first().address.toInt(), 1).first(), true)
     }
+
+    @Test
+    fun `Set must set a INT16 value to a holding register variable`() {
+        val configuration = ConfigurationParser()
+        configuration.setReadFromResources(true)
+        configuration.setFileName("set_operation.xml")
+        val plcMemory = PlcMemory(configuration)
+        val setOperation = SetOperation(configuration.getConfiguredDevice().configuration, plcMemory, EnvironmentVariables(listOf<EnvParameter>(),ConfigurationParser() ))
+        assertTrue(configuration.getConfiguredDevice().simulation.randomElements[1] is Set)
+        assertEquals(plcMemory.readHoldingRegister(configuration.getConfiguredDevice().configuration.registers.register[1].address.toInt(), 1).first(), 500)
+        setOperation.setOperation(configuration.getConfiguredDevice().simulation.randomElements[1] as Set)
+        assertEquals(plcMemory.readHoldingRegister(configuration.getConfiguredDevice().configuration.registers.register[1].address.toInt(), 1).first(), 600)
+    }
+
 }
